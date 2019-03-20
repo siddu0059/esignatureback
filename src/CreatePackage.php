296fcc12dc87de5464af9a162be72667905dd4e2
shanth kumar <shanth@example.com>
2019-03-20 13:04:55 +0530

    esignture

--- src/CreatePackage.php
+++ src/CreatePackage.php
@@ -4,47 +4,31 @@ interface ICreatePackage {
     function Package();
 }
 class CreatePackage implements ICreatePackage {
-    private $DocumentPath,$Initiator,$CallBackUrl,$DocumentLanguage,$DocumentName,$NotificationCallBackUrl,$DocumentGroupCode,$ExpiryTimestamp,$ExternalPackageReference,$ExternalPackageData,$F2FRedirectUrl,$ExternalDocumentReference,$Stakeholders,$TargetType,$PdfErrorHandling,$SigningTemplateCode,$CorrelationId;
-    public function __construct($DocumentPath,$Initiator,$CallBackUrl,$DocumentLanguage,$DocumentName,$NotificationCallBackUrl,$DocumentGroupCode,$ExpiryTimestamp,$ExternalPackageReference,$ExternalPackageData,$F2FRedirectUrl,$ExternalDocumentReference,$StakeholderFirst,$StakeholderSecond,$TargetType,$PdfErrorHandling,$SigningTemplateCode,$CorrelationId) {
-        $this->DocumentPath = $DocumentPath;
-        $this->Initiator = $Initiator;
-        $this->CallBackUrl = $CallBackUrl;
-        $this->DocumentLanguage = $DocumentLanguage;
-        $this->DocumentName = $DocumentName;
-        $this->NotificationCallBackUrl = $NotificationCallBackUrl;
-        $this->DocumentGroupCode = $DocumentGroupCode;
-        $this->ExpiryTimestamp = $ExpiryTimestamp;
-        $this->ExternalPackageReference = $ExternalPackageReference;
-        $this->ExternalPackageData = $ExternalPackageData;
-        $this->F2FRedirectUrl = $F2FRedirectUrl;
-        $this->ExternalDocumentReference = $ExternalDocumentReference;
-        $this->StakeholderFirst = $StakeholderFirst;
-        $this->StakeholderSecond = $StakeholderSecond;
-        $this->TargetType = $TargetType;
-        $this->PdfErrorHandling = $PdfErrorHandling;
-        $this->SigningTemplateCode = $SigningTemplateCode;
-        $this->CorrelationId = $CorrelationId;
+    private $PackageInfo,$Stakeholders;
+    public function __construct($PackageInfo,$Stakeholders) {
+        $this->PackageInfo = $PackageInfo;
+        $this->Stakeholders = $Stakeholders;
     }
     public function Package() {
-        $Document = base64_encode(file_get_contents($this->DocumentPath));
+        $Document = $this->PackageInfo['PdfData'];
         $Package = [
             "Document"=>$Document,
-            "Initiator"=>$this->Initiator,
-            "CallBackUrl"=>$this->CallBackUrl,
-            "DocumentLanguage"=>$this->DocumentLanguage,
-            "DocumentName"=>$this->DocumentName,
-            "NotificationCallBackUrl"=>$this->NotificationCallBackUrl,
-            "DocumentGroupCode"=>$this->DocumentGroupCode,
-            "ExpiryTimestamp"=>$this->ExpiryTimestamp,
-            "ExternalPackageReference"=>$this->ExternalPackageReference,
-            "ExternalPackageData"=>$this->ExternalPackageData,
-            "F2FRedirectUrl"=>$this->F2FRedirectUrl,
-            "ExternalDocumentReference"=>$this->ExternalDocumentReference,
-            "Stakeholders"=>[$this->StakeholderFirst,$this->StakeholderSecond],
-            "TargetType" =>$this->TargetType,
-            "PdfErrorHandling" =>$this->PdfErrorHandling,
-            "SigningTemplateCode" =>$this->SigningTemplateCode,
-            "CorrelationId" =>$this->CorrelationId,
+            "Initiator"=>$this->PackageInfo['Initiator'],
+            "CallBackUrl"=>$this->PackageInfo['CallBackUrl'],
+            "DocumentLanguage"=>$this->PackageInfo['DocumentLanguage'],
+            "DocumentName"=>$this->PackageInfo['DocumentName'],
+            "NotificationCallBackUrl"=>$this->PackageInfo['NotificationCallBackUrl'],
+            "DocumentGroupCode"=>$this->PackageInfo['DocumentGroupCode'],
+            "ExpiryTimestamp"=>$this->PackageInfo['ExpiryTimestamp'],
+            "ExternalPackageReference"=>$this->PackageInfo['ExternalPackageReference'],
+            "ExternalPackageData"=>$this->PackageInfo['ExternalPackageData'],
+            "F2FRedirectUrl"=>$this->PackageInfo['F2FRedirectUrl'],
+            "ExternalDocumentReference"=>$this->PackageInfo['ExternalDocumentReference'],
+            "Stakeholders"=>$this->Stakeholders,
+            "TargetType" =>$this->PackageInfo['TargetType'],
+            "PdfErrorHandling" =>$this->PackageInfo['PdfErrorHandling'],
+            "SigningTemplateCode" =>$this->PackageInfo['SigningTemplateCode'],
+            "CorrelationId" =>$this->PackageInfo['CorrelationId'],
         ];
         return $Package;
     }
